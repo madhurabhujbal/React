e455b438b900b5ed7ee65c409b2c176699cf0d65
Madhura <madhura.bhujbal@gmail.com>
2020-07-21 22:38:55 +0100

    Add code to display hello world on page

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react"; //React is object and react is module
 import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
-console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
